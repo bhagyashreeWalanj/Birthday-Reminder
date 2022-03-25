1e0b0c1505870780ad0fac7f986b33edc2a6e397
bhagyashreeWalanj <shree.6490@gmail.com>
2022-03-25 19:03:22 +0100

    Added new project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import data from './mockup';
+import List from './List';
 
 function App() {
+  const [people, setPeople] = useState(data)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className='container'>
+        <h3>{people.length} Birthdays today</h3>
+        <div role="listitem"><List people={people} /></div>
+        <button onClick={() => setPeople([])} >clear all</button>
+        <button onClick={() => setPeople(data)}>Reset</button>
+      </section>
+    </main>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,67 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import axios from 'axios';
 import App from './App';
+jest.mock('axios');
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe('Run Birthday reminder App without Crashing', () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  test('should display all birthday list after render', () => {
+    const items = screen.getAllByRole('heading', { level: 4 });
+    expect(items).toHaveLength(5);
+  });
+
+  test('should clear all list from app container', async () => {
+
+    userEvent.click(screen.getByRole('button', { name: 'clear all' }));
+    expect(screen.queryByText(/Bertie/i)).not.toBeInTheDocument(); // check element not exist in document
+    expect(screen.queryByRole('heading', { level: 4 })).toBeNull(); // element doesn't exist
+    expect(screen.queryAllByRole('heading', { level: 4 })).toHaveLength(0); //// expect no elements
+
+  });
+
+  test('should reset all the birthday reminders', () => {
+    userEvent.click(screen.getByRole('button', { name: 'Reset' }));
+    expect(screen.queryAllByRole('heading', { level: 4 })).toHaveLength(5);
+  });
+
+  test('should display mock data on screen', async () => {
+    const stories = [
+      {
+        id: 1,
+        name: 'Bhagyashrre Yates',
+        age: 29,
+        image:
+          'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+      },
+      {
+        id: 2,
+        name: 'sangam Hogan',
+        age: 32,
+        image:
+          'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+      },
+    ];
+
+    const promise = Promise.resolve({ data: stories });
+    axios.get.mockImplementationOnce(() => promise);
+    // render(<App />);
+    await userEvent.click(screen.getByRole('button', { 'name': 'Reset' }));
+
+    await act(() => promise);
+
+    expect(screen.getAllByRole('heading', { level: 4 })).toHaveLength(5);
+
+
+  })
+
+
+
 });
+
+
